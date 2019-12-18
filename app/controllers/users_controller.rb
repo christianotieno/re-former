380e6fce12e0994b21ab9dda9ac7e601cd3845c5
Tommy M0ntana <tommy.montana.harwood@gmail.com>
2019-12-18 19:03:34 +0100

    add param calls to user create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+  
+  if @user.save
+    redirect_to new_user_path
+  else
+    render :new
+   end
+   end
+
+private
+
+def user_params
+  params.required(:user).permit(:username, :email, :password)
+end
 end
