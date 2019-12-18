171cf500e8d184e2ebe18a2d9f7802d52145609e
ChristianOtieno <cgao273@gmail.com>
2019-12-18 22:23:00 +0300

    SET UP rubocop code linter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-  
-  if @user.save
-    redirect_to new_user_path
-  else
-    render :new
-   end
-   end
 
-private
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
-def user_params
-  params.required(:user).permit(:username, :email, :password)
-end
+  private
+
+  def user_params
+    params.required(:user).permit(:username, :email, :password)
+  end
 end
